693051a664d5baa69417c27d4f647eadf4d3793e
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-12-11 18:36:54 +0530

    [DA] stateless widgets

--- codebase/flutter-in-action/03-dog-app/lib/my_home_page.dart
+++ codebase/flutter-in-action/03-dog-app/lib/my_home_page.dart
@@ -8,9 +8,55 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dog App'),
+        title: const Text('Yellow Lab'),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            DogName(
+              dogName: 'Rocky',
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            DogName(
+              dogName: 'Bruno',
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            DogName(
+              dogName: 'Nico',
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DogName extends StatelessWidget {
+  const DogName({
+    Key? key,
+    required this.dogName,
+  }) : super(key: key);
+
+  final String dogName;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        color: Colors.lightBlueAccent,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(dogName),
+      ),
     );
   }
 }
