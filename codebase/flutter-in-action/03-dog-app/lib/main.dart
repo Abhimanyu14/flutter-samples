f2bdf2bcacf939b49598fb131a3fb3f8e29046b3
Abhimanyu14 <abhimanyu.n14@gmail.com>
2021-12-11 18:05:17 +0530

    [DA] created dog app project

---
+++ codebase/flutter-in-action/03-dog-app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:dog_app/my_home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Dog App',
+      home: MyHomePage(),
+    );
+  }
+}
